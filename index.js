179b49d709cfb890e3ea22e300e60a4f558e83de
Natalie Ruiz & Thomas Feng <pair+nataliecita.realtomfeng@devbootcamp.com>
2017-06-05 11:27:53 -0400

    Grab value from box

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
-console.log("hello")
+document.getElementById("input-box").addEventListener("keypress", (e) => {
+  var key = e.which || e.keyCode;
+  if (key ===13){
+    let input = document.getElementById("input-box").value
+    console.log(input)
+  }
+})
