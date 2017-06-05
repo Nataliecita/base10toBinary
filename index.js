1886937c66b93344ea2e04e2c1d4aba51563b09d
Natalie Ruiz & Thomas Feng <pair+nataliecita.realtomfeng@devbootcamp.com>
2017-06-05 12:11:47 -0400

    Correct binary string

--- index.js
+++ index.js
@@ -1,7 +1,22 @@
 document.getElementById("input-box").addEventListener("keypress", (e) => {
   var key = e.which || e.keyCode;
   if (key ===13){
-    let input = document.getElementById("input-box").value
-    console.log(input)
+    let box = document.getElementById("input-box")
+    let input = box.value
+    let binary = binary_convert(input)
+    console.log(binary)
+    box.value = binary;
   }
 })
+
+var binary_convert = function(input, binary= "") {
+  if (input > 1) {
+    binary = (input % 2) + binary;
+
+    input = Math.floor(input/2);
+    return binary_convert(input, binary);
+  }
+  else {
+    return (1 + binary);
+  }
+}
