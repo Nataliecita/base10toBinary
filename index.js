0315df2df0e18c61c054f0ab5014072ecbefbfa9
Natalie Ruiz & Thomas Feng <pair+nataliecita.realtomfeng@devbootcamp.com>
2017-06-05 15:29:28 -0400

    Complete widget

--- index.js
+++ index.js
@@ -3,7 +3,21 @@ console.log("dfdsgdfgh")
 class ConvertToBinary {
   constructor(domHook){
     this.elem = document.getElementById(domHook)
+    // this.props;
+    // this.state = "";
+    this.elem.addEventListener("keypress", (e) => {
+      var key = e.which || e.keyCode;
+      if (key ===13){
+        let box = document.getElementById("input-box")
+        let input = box.value
+        let binary = this.binary_convert(input)
+        box.value = binary;
+      }
+    })
     this.render();
+  }
+  ConvertToBinary() {
+    this.render()
   }
 
   render() {
@@ -11,36 +25,19 @@ class ConvertToBinary {
 
     this.elem.innerHTML = body;
   }
+  binary_convert(input, binary= "") {
+    if (input > 1) {
+      binary = (input % 2) + binary;
+
+      input = Math.floor(input/2);
+      return this.binary_convert(input, binary);
+    }
+    else {
+      return (1 + binary);
+    }
+  }
 }
 
 
 
-
-
-// document.getElementById("input-box").addEventListener("keypress", (e) => {
-//   var key = e.which || e.keyCode;
-//   if (key ===13){
-//     let box = document.getElementById("input-box")
-//     let input = box.value
-//     let binary = binary_convert(input)
-//     console.log(binary)
-//     box.value = binary;
-//   }
-// })
-
-
-
 new ConvertToBinary("input-box-hook");
-
-
-// var binary_convert = function(input, binary= "") {
-//   if (input > 1) {
-//     binary = (input % 2) + binary;
-//
-//     input = Math.floor(input/2);
-//     return binary_convert(input, binary);
-//   }
-//   else {
-//     return (1 + binary);
-//   }
-// }
