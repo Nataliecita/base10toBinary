5a2704d9e65265ccc3635396e279aedb47be95cf
Natalie Ruiz & Thomas Feng <pair+nataliecita.realtomfeng@devbootcamp.com>
2017-06-05 16:17:29 -0400

    Change to use state

--- index.js
+++ index.js
@@ -3,34 +3,33 @@ console.log("dfdsgdfgh")
 class ConvertToBinary {
   constructor(domHook){
     this.elem = document.getElementById(domHook)
-    // this.props;
-    // this.state = "";
+    this.state = {
+      number: 0};
+      this.render();
     this.elem.addEventListener("keypress", (e) => {
       var key = e.which || e.keyCode;
       if (key ===13){
-        let box = document.getElementById("input-box")
-        let input = box.value
-        let binary = this.binary_convert(input)
-        box.value = binary;
+        this.state.number = document.getElementById("input-box").value
+        this.ConvertToBinary()
       }
     })
-    this.render();
   }
   ConvertToBinary() {
+    this.state.number = this.binaryConvert(this.state.number)
     this.render()
   }
 
   render() {
-    let body = `<input id="input-box" type='text' placeholder='This is a box'>`
+    let body = `<input id="input-box" type='text' value=${this.state.number}>`
 
     this.elem.innerHTML = body;
   }
-  binary_convert(input, binary= "") {
+  binaryConvert(input, binary= "") {
     if (input > 1) {
       binary = (input % 2) + binary;
 
       input = Math.floor(input/2);
-      return this.binary_convert(input, binary);
+      return this.binaryConvert(input, binary);
     }
     else {
       return (1 + binary);
