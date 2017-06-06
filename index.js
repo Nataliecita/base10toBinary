64616ad3ce0cba626d4814e3430a5c62a6f7f3c5
Natalie Ruiz & Thomas Feng <pair+nataliecita.realtomfeng@devbootcamp.com>
2017-06-05 20:53:48 -0400

    refactored

--- index.js
+++ index.js
@@ -1,40 +1,60 @@
-console.log("dfdsgdfgh")
-
-class ConvertToBinary {
-  constructor(domHook){
-    this.elem = document.getElementById(domHook)
+class ParentWidget {
+  constructor() {
+    this.elem = document.getElementById("parentInput")
+    this.props = {
+      childWidgets: []
+    }
     this.state = {
-      number: 0};
-      this.render();
+      number: ""
+    }
     this.elem.addEventListener("keypress", (e) => {
       var key = e.which || e.keyCode;
-      if (key ===13){
-        this.state.number = document.getElementById("input-box").value
-        this.ConvertToBinary()
+      if (key === 13){
+        this.state.number = this.elem.value
+        this.render()
       }
     })
   }
-  ConvertToBinary() {
-    this.state.number = this.binaryConvert(this.state.number)
-    this.render()
+  register(child) {
+    this.props.childWidgets.push(child);
+    child.props.parentWidget = this;
+    child.render()
   }
-
   render() {
-    let body = `<input id="input-box" type='text' value=${this.state.number}>`
-
-    this.elem.innerHTML = body;
+    this.props.childWidgets.forEach(c => c.ConvertToBinary(this.state.number));
   }
-  binaryConvert(input, binary= "") {
-    if (input > 1) {
-      binary = (input % 2) + binary;
 
-      input = Math.floor(input/2);
-      return this.binaryConvert(input, binary);
-    }
-    else {
-      return (1 + binary);
-    }
-  }
 }
 
-new ConvertToBinary("input-box-hook");
+
+class ConvertToBase {
+  constructor(domHook, base){
+    this.base = base;
+    this.elem = document.getElementById(domHook)
+    this.state = {
+      number: ""
+    };
+    this.props = {
+    };
+  }
+  ConvertToBinary(number) {
+    this.state.number = this.baseConvert(number)
+    debugger
+    this.render()
+  }
+  render() {
+    let body = `<input id="input-box" type='text' value=${this.state.number}>`
+    this.elem.innerHTML = body;
+  }
+  baseConvert(input) {
+    return (input >>> 0).toString(this.base);
+}
+}
+
+let ctb2 = new ConvertToBase("base2", 2);
+let ctb8 = new ConvertToBase("base8", 8);
+let ctb16 = new ConvertToBase("base16", 16);
+let parent = new ParentWidget()
+parent.register(ctb2);
+parent.register(ctb8);
+parent.register(ctb16);
